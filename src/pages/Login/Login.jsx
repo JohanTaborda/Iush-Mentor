cb25876b77041456394b6fc7ea9c8ff488e208ba
samuelarredondo <arredondosamuel0@gmail.com>
2025-04-04 21:20:31 -0500

    Merge pull request #8 from feature/06-Adjust-Login
    * Se organizan detalles generales del login
    
    * Se organiza tamaño de los errores al iniciar sesión y demas
    
    * Inicio de sesión funcional.
    
    * Se corrige el condicional que permite la visibilidad del login en el mainpanel.
    
    * Se organiza el texto de '¿No tienes cuenta?' en dispositivos móviles.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,71 +1,89 @@
-import React, { useState } from "react"; // Para utilizar React
-import "./Login"  // Funciones personalizadas para el componente Login.
-import "./Login.css"  // Estilos personalizados para el componente Login.
-import { FaRegEyeSlash } from "react-icons/fa6"; // Importar icono de ojo cerrado
-import { FaRegEye } from "react-icons/fa"; // Importar Icono de ojo abierto
-import { GiGraduateCap } from "react-icons/gi"; // Importar Icono de graduación
+import React, { useState } from "react"; 
+import "./Login";  
+import "./Login.css";  
+import { FaRegEyeSlash } from "react-icons/fa6"; 
+import { FaRegEye } from "react-icons/fa"; 
+import { GiGraduateCap } from "react-icons/gi"; 
+import { Modal } from "bootstrap";
+import { IoIosClose } from "react-icons/io";
 
-const Login = ({setVisRegister,setMainComponent}) => { //Lo que permite que el botón iniciar sesión funcione
+const Login = ({ setVisRegister, setMainComponent, setVisAuth }) => { 
 
-    // Variables.
-    const validasrUsuario = "iush-mentor@gmail.com" // correo para inicira sesión
-    const validarContraseña = "iush-mentor"// correo para iniciar sesión
-    // Constantes.
-    const [visOverlay, setVisOverlay] = useState(false) // Overlay para la vista del Login
-    const [user, setUser] = useState("") // estado para inicializar el usuario
-    const [password, setPassword] = useState("") // estado para inicializar la contraseña
-    const [error, setError] = useState(false) // estado para inicializar error
-    const [showPwd, setShowPdw] = useState(true)//Estado para inicializar la función para que cambie l ojo y la forma de digitar la contraseña
-    const handleSubmit = (e) =>{ //Para manejar el formulario 
-        e.preventDefault()
+    const validarUsuario = "iush-mentor@gmail.com";
+    const validarContraseña = "iush-mentor";
 
-        if(user == "" || password == ""){ // sentencia para definir si el usuario o la contraseña estan vacios
-            setError(true) // Si hay algun campo vacio el setError se vuelve verdadero
-            return
+    const [user, setUser] = useState(""); 
+    const [password, setPassword] = useState(""); 
+    const [errors, setErrors] = useState("");  
+    const [showPwd, setShowPwd] = useState(true);  
+    const [closeButton, setCloseButton] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!password.trim() && !user.trim()) {
+            setErrors("Credenciales vacías");
+        } else if (!password.trim()) {
+            setErrors("Contraseña obligatoria");
+        } else if (!user.trim()) {
+            setErrors("Correo obligatorio");
+        } else if (password !== validarContraseña || user !== validarUsuario) {
+            setErrors("Credenciales incorrectas");
         }
-        if(user == validasrUsuario && password == validarContraseña){ //Permite validar la informacion para poder que se ingrese al Navbar
-            setMainComponent(true) // si se cumple la sentencia se sigue al Navbar
-        }
-    }
 
-    const icon__eyeEnable = () => <FaRegEye onClick={() => setShowPdw(!showPwd)} className="icons_eyes"/> // función donde se da a conocer que si se le da click al ojo va a cambiar de icono por el ojo cerrado ademas que se muestra el icono
-    const icon__eyeDisabled = () => <FaRegEyeSlash onClick={() => setShowPdw(!showPwd)} className="icons_eyes"/> // función donde se da a conocer que si se le da click al ojo va a cambiar de icono por el ojo abierto ademas que se muestra el icono
-    const icon__cap = () => < GiGraduateCap className="icons_eyes" color="blue"/> // se muestra el logo de la graduación con su respectivo color
-    
-    return(
+        if(user == validarUsuario && password == validarContraseña){ 
+            setMainComponent(true)
+        }
+    };
+
+    const icon__eyeEnable = () => (
+        <FaRegEye onClick={() => setShowPwd(!showPwd)} className="icons_eyes" />
+    );
+
+    const icon__eyeDisabled = () => (
+        <FaRegEyeSlash onClick={() => setShowPwd(!showPwd)} className="icons_eyes" />
+    );
+
+    const icon__cap = () => <GiGraduateCap className="icons_eyes" color="blue" />;
+
+    const icon__close = () => <IoIosClose className="icon_close" color="#000" onClick={() => setVisAuth(false)}/>;
+
+    return (
         <>
-            <div className="overlay"> {/* contenedor donde se encuentra el overlay*/}
-                <div className="Login__container"> {/*contenedor donde se encuentra el login */}
-                    <div className="Login__img"> {/* contenedor donde se muestra la imagen */}
-                        <img src="src/resources/images/logoAplication/Logo-IushMentor.png" alt="img-mentor" className="img_IUSH" /> {/* se agrega la imagen al login con su respectiva clase*/}
+            <div className="overlay">
+                <div className="Login__container">
+                    <div className="Login__img">
+                        <img src="src/resources/images/logoAplication/Logo-IushMentor.png" 
+                             alt="img-mentor" className="img_IUSH" />
                     </div>
-                    <div className="Login__welcome"> {/*contenedor de la parte derecha del ogin, quiere decir la de bienvenida */}
-                                <div className="Login_WelcomeInitial"> {/* contenedor donde se muestra la parte inicial del login*/}
-                                    <h3 className="Login_Welcometittle">Bienvenid@s:</h3>
-                                    <p className="mss_top">Accede a nuestra comunidad de mentores expertos en diseño, tecnología, inteligencia artificial y más. {icon__cap()} </p> {/*Aca se esra invocando el icono de graduacion */}
-                                </div>
-                                <form action="login_formulario" onSubmit={handleSubmit} className="login_forms"> {/* Aca se inicializa el formulario y se llama la función handlesubmit para que se reinicie el formulario*/}
-                                    <input type="email" value={user} onChange={(e) => setUser(e.target.value)} placeholder="Email" className="" id="LoginInput"/> {/* esta linea de codigo nos dice que estamos inicializando el input y que tipo de dato se debe digitar, ademas el e.target.value es para que se guarden los datos que se digitan*/}
-                                    <div className="container__password"> {/* contenedor que se hace para poder posicionar el icono del ojo y donde esta la parte de la password*/}
-                                        <input type={!showPwd ? "text" : "password"} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña"className="input__password" id="LoginInput"/> {/*En esta linea se hace una sentencia para que en el input se pueda cambiar la forma de ver los datos, ya sea que el usuario lo quiera ver con puntos o la contraseña en si*/}
-                                        <span id="ubicacion__icon">{ !showPwd ? icon__eyeEnable(): icon__eyeDisabled() } </span> {/* aca se hace la funcion para permitir que el icono vaya cambiando de forma a medida que se le da click, tambien se cambia el formato en el que se ve la contraseña*/}
-                                    </div>
-                                    <div> {/* se inicia contenedor para mejorar la imagen visualmente del boton*/}
-                                    <button id="BtnWelcome">INGRESAR</button> 
-                                    {error && <p className="Login_error">Todos los campos son obligatorios</p>} {/* sentencia donde se muestra el error si algun campo de los input, sea email o password estan vacios*/}
-                                    </div> {/* contenedor para darle una mejor organizacion al apartado registro*/}
-                                    <div className="Login__Register">
-                                        <p className="Login_not">¿No tienes cuenta? </p>
-                                    <a onClick={() => setVisRegister(false)} id="click_register" >Registráte</a> {/*donde se muestra el apartado de registrarse y muestra a donde se redirecciona al darle click ese apartado */}
-                                    </div>                                    
-                                </form>
-                                
+                    <div className="Login__welcome">
+                    {icon__close()}
+                        <div className="Login_WelcomeInitial">
+                            <h3 className="Login_Welcometittle">Bienvenid@s:</h3>
+                            <p className="mss_top">Accede a nuestra comunidad de mentores expertos en diseño, tecnología, inteligencia artificial y más. {icon__cap()} </p>
+                        </div>
+
+                        <form action="login_formulario" onSubmit={handleSubmit} className="login_forms">
+                            <input type="text" value={user} onChange={(e) => setUser(e.target.value)} placeholder="Email" className="input_Credential"/>
+                            <div className="container__password">
+                                <input type={showPwd ? "password" : "text"} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" className="input_Credential"/>
+                                <span id="ubicacion__icon">{showPwd ? icon__eyeDisabled() : icon__eyeEnable()}</span>
+                            </div>
+                            <div>
+                                <button id="BtnWelcome" >INGRESAR</button> 
+                            </div>
+                                <p className="Login_error">{errors}</p>
+                            <div className="Login__Register">
+                                <p className="Login_not">¿No tienes cuenta? </p>
+                                <a onClick={() => setVisRegister(false)} id="click_register">
+                                    Regístrate
+                                </a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>    
-
         </>
-    )
-}
+    );
+};
 
 export default Login;
